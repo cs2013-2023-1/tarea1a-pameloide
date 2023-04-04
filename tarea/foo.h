fc4547d95634b748f5d6d68758dffd68a916322c
pameloide <129806557+pameloide@users.noreply.github.com>
2023-04-04 11:28:06 -0500

    Update foo.h

--- tarea/foo.h
+++ tarea/foo.h
@@ -1,17 +1,21 @@
+//
+// Created by Camila on 04/04/2023.
+//
+
 #include <iostream>
 
 using namespace std;
 
 class Matriz2D{
-friend Matriz2D t(Matriz2D&);
-friend ostream& operator<<(ostream&, const Matriz2D&);
-friend Matriz2D operator+(const Matriz2D&, const Matriz2D&);
-friend Matriz2D operator-(const Matriz2D&, const Matriz2D&);
-friend Matriz2D operator*(const Matriz2D&, const Matriz2D&);
-friend Matriz2D operator+(const Matriz2D&, float);
-friend Matriz2D operator-(const Matriz2D&, float);
-friend Matriz2D operator*(const Matriz2D&, float);
-friend Matriz2D operator/(const Matriz2D&, float);
+    friend Matriz2D t(Matriz2D&);
+    friend ostream& operator<<(ostream&, const Matriz2D&);
+    friend Matriz2D operator+(const Matriz2D&, const Matriz2D&);
+    friend Matriz2D operator-(const Matriz2D&, const Matriz2D&);
+    friend Matriz2D operator*(const Matriz2D&, const Matriz2D&);
+    friend Matriz2D operator+(const Matriz2D&, float);
+    friend Matriz2D operator-(const Matriz2D&, float);
+    friend Matriz2D operator*(const Matriz2D&, float);
+    friend Matriz2D operator/(const Matriz2D&, float);
 public:
     Matriz2D();
     Matriz2D(int);
@@ -28,3 +32,12 @@ private:
     int columnas;
 };
 
+Matriz2D t(Matriz2D&);
+ ostream& operator<<(ostream&, const Matriz2D&);
+ Matriz2D operator+(const Matriz2D&, const Matriz2D&);
+Matriz2D operator-(const Matriz2D&, const Matriz2D&);
+ Matriz2D operator*(const Matriz2D&, const Matriz2D&);
+Matriz2D operator+(const Matriz2D&, float);
+Matriz2D operator-(const Matriz2D&, float);
+Matriz2D operator*(const Matriz2D&, float);
+Matriz2D operator/(const Matriz2D&, float);
